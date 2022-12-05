cd50200f7ab477446ce4af24350eb840f78b8344
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2022-12-05 20:25:04 +0200

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,135 @@
+
+const body = document.querySelector('body'),
+    html = document.querySelector('html'),
+    menu = document.querySelectorAll('.header__burger, .header__nav, body'),
+    burger = document.querySelector('.header__burger'),
+    header = document.querySelector('.header');
+
+
+body.addEventListener('click', function (event) {
+
+    function $(elem) {
+      return event.target.closest(elem)
+    }
+
+    // =-=-=-=-=-=-=-=-=-=- <Open/Close menu in header> -=-=-=-=-=-=-=-=-=-=-
+
+    if ($('.header__burger')) {
+        menu.forEach(element => {
+            element.classList.toggle('_active')
+        })
+    }
+
+    // =-=-=-=-=-=-=-=-=-=- </Open/Close menu in header> -=-=-=-=-=-=-=-=-=-=-
+
+
+
+    // =-=-=-=-=-=-=-=-=-=- <Custom select in header> -=-=-=-=-=-=-=-=-=-=-
+
+    const headerNavSelectTarget = $('.header__nav--select-target')
+    if (headerNavSelectTarget) {
+
+      headerNavSelectTarget.classList.toggle('_active')
+
+    } else if(!$('.header__nav--select')) {
+
+      document.querySelectorAll('.header__nav--select-target._active').forEach(headerNavSelectTarget => {
+        headerNavSelectTarget.classList.remove('_active')
+      })
+
+    }
+
+  // =-=-=-=-=-=-=-=-=-=- </Custom select in header> -=-=-=-=-=-=-=-=-=-=-
+
+})
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=- <resize> -=-=-=-=-=-=-=-=-=-=-=-=
+
+let resizeCheck = {}, windowSize;
+
+function resizeCheckFunc(size, minWidth, maxWidth) {
+  if (windowSize <= size && (resizeCheck[String(size)] == true || resizeCheck[String(size)] == undefined) && resizeCheck[String(size)] != false) {
+    resizeCheck[String(size)] = false;
+    maxWidth(); // < size
+  }
+
+  if (windowSize >= size && (resizeCheck[String(size)] == false || resizeCheck[String(size)] == undefined) && resizeCheck[String(size)] != true) {
+    resizeCheck[String(size)] = true;
+    minWidth(); // > size
+  }
+}
+
+function resize() {
+
+  windowSize = window.innerWidth
+  html.style.setProperty('--height-screen', window.innerHeight + 'px');
+  html.style.setProperty('--height-header', header.offsetHeight + 'px');
+
+  /* resizeCheckFunc(992,
+    function () {  // screen > 992px
+
+
+
+  },
+  function () {  // screen < 992px
+
+
+
+  }); */
+
+}
+
+resize();
+
+window.onresize = resize;
+
+// =-=-=-=-=-=-=-=-=-=-=-=- </resize> -=-=-=-=-=-=-=-=-=-=-=-=
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=- <slider> -=-=-=-=-=-=-=-=-=-=-=-=
+/*
+let slider = new Swiper('.__slider', {
+  
+    spaceBetween: 30,
+    slidesPerView: 1,
+    centeredSlides: false,
+
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 3,
+        centeredSlides: true,
+    
+      },
+      600: {
+        slidesPerView: 2,
+        centeredSlides: false,
+      },
+    }
+}); 
+*/
+// =-=-=-=-=-=-=-=-=-=-=-=- </slider> -=-=-=-=-=-=-=-=-=-=-=-=
+
+
+/* 
+// =-=-=-=-=-=-=-=-=-=-=-=- <Анимации> -=-=-=-=-=-=-=-=-=-=-=-=
+
+wow = new WOW({
+mobile:       false,
+})
+wow.init();
+
+// =-=-=-=-=-=-=-=-=-=-=-=- </Анимации> -=-=-=-=-=-=-=-=-=-=-=-=
+
+*/
